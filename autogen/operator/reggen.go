0c179c087a9d0c903a84eaf7bab2d19a8aa8b3b2
Zixuan Guo <v-zixguo@microsoft.com>
2021-04-20 10:46:51 +0800

    change reggen generation algorithm

--- autogen/operator/reggen.go
+++ autogen/operator/reggen.go
@@ -1,10 +1,32 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+// The MIT License (MIT)
+// Copyright (C) 2016 Lucas Jones
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 package operator
 
 import (
+	"errors"
+	"log"
 	"math"
+	"regexp"
 	"regexp/syntax"
 	"strings"
 
@@ -12,167 +34,168 @@ import (
 )
 
 const (
-	randomStringLimit    = 5
-	repeatedstringLimit  = 10
-	negatedProb          = 0.1
-	printableChars       = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-	printableCharsLower  = 33  // Lowest byte value of printable chars
-	printableCharsUppper = 126 // higher byte value of printable chars
+	maxRetry            = 50
+	repeatedstringLimit = 10
+	printableChars      = "!\"#$%&'()*+,-./0123456789:<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+	runeRangeEnd        = 0x10ffff
 )
 
-func randomString(length int) string {
-	var builder strings.Builder
-	for i := 0; i < length; i++ {
-		builder.WriteByte(printableChars[utils.RandomIntWithRange(0, len(printableChars))])
-	}
-	return builder.String()
-}
+var ErrFailedGeneration = errors.New("autogen/operator: Unable to generation string from regexp")
 
-func randomStringWithRange(ranges []rune) string {
-	sum := 0
-	for i := 0; i < len(ranges); i += 2 {
-		sum += int((ranges[i+1] - ranges[i] + 1))
-	}
-	randomIndex := utils.RandomIntWithRange(0, sum)
-	for i := 0; i < len(ranges); i += 2 {
-		diff := int(ranges[i+1]-ranges[i]) + 1
-		if randomIndex < diff {
-			return string(ranges[i] + int32(randomIndex))
-		}
-		randomIndex -= diff
-	}
-	panic("randomIndex out of range")
-}
-
-func repeatSubexpression(re *syntax.Regexp, times int, not bool) (string, bool) {
-	var builder strings.Builder
-	isNegated := false
-	for i := 0; i < times; i++ {
-		for _, r := range re.Sub {
-			// execute sub-expression
-			res, isChildNegated := generate(r, not)
-			builder.WriteString(res)
-			isNegated = isNegated || isChildNegated
-		}
-	}
-	return builder.String(), isNegated
-}
-
-func generate(re *syntax.Regexp, not bool) (string, bool) {
-	isNegated := utils.RandomBiasedBool(negatedProb) && not // only for non-meta operation
-
-	switch re.Op {
-	case syntax.OpNoMatch, syntax.OpEmptyMatch:
-		// Return a random string for negated operation
-		// Return an empty string otherwise
-		if isNegated {
-			return randomString(randomStringLimit), isNegated
-		}
-		return "", isNegated
+func generate(re *syntax.Regexp) string {
+	//fmt.Println("re:", re, "sub:", re.Sub)
+	op := re.Op
+	switch op {
+	case syntax.OpNoMatch:
+	case syntax.OpEmptyMatch:
+		return ""
 	case syntax.OpLiteral:
-		// Return a string with one Rune randomly skipped for negated operation
-		// Return the original literal otherwise
-		var builder strings.Builder
-		if isNegated {
-			skippedRuneIndex := utils.RandomIntWithRange(0, len(re.Rune))
-			for index, r := range re.Rune {
-				if index != skippedRuneIndex {
-					builder.WriteRune(r)
-				}
-			}
-		} else {
-			for _, r := range re.Rune {
-				builder.WriteRune(r)
-			}
+		res := ""
+		for _, r := range re.Rune {
+			res += string(r)
 		}
-		return builder.String(), isNegated
+		return res
 	case syntax.OpCharClass:
-		validRunes := make([]rune, 0)
+		// number of possible chars
+		sum := 0
 		for i := 0; i < len(re.Rune); i += 2 {
-			if re.Rune[i] > re.Rune[i+1] { // sanity check
-				continue
+			sum += int(re.Rune[i+1]-re.Rune[i]) + 1
+			if re.Rune[i+1] == runeRangeEnd {
+				sum = -1
+				break
 			}
-			if printableCharsLower <= re.Rune[i] && re.Rune[i+1] <= printableCharsUppper {
-				validRunes = append(validRunes, re.Rune[i], re.Rune[i+1])
-			} else {
-				var lb, hb int32 // rune
-				lb = int32(math.Max(float64(re.Rune[i]), float64(printableCharsLower)))
-				hb = int32(math.Min(float64(re.Rune[i+1]), float64(printableCharsUppper)))
-				if lb > hb { // when range [re.Runep[i], re.Rune[i+1]] does not overlap w/ [printableL, printableU]
-					continue
+		}
+		// pick random char in range (inverse match group)
+		if sum == -1 {
+			possibleChars := []uint8{}
+			for j := 0; j < len(printableChars); j++ {
+				c := printableChars[j]
+				//fmt.Printf("Char %c %d\n", c, c)
+				// Check c in range
+				for i := 0; i < len(re.Rune); i += 2 {
+					if rune(c) >= re.Rune[i] && rune(c) <= re.Rune[i+1] {
+						possibleChars = append(possibleChars, c)
+						break
+					}
 				}
-				validRunes = append(validRunes, lb, hb)
+			}
+			if len(possibleChars) > 0 {
+				c := possibleChars[utils.RandomIntWithRange(0, len(possibleChars))]
+				return string([]byte{c})
 			}
 		}
-
-		if len(validRunes) == 0 { // TODO: better way of handling this
-			return "", false
+		r := utils.RandomIntWithRange(0, int(sum))
+		var ru rune
+		sum = 0
+		for i := 0; i < len(re.Rune); i += 2 {
+			gap := int(re.Rune[i+1]-re.Rune[i]) + 1
+			if sum+gap > r {
+				ru = re.Rune[i] + rune(r-sum)
+				break
+			}
+			sum += gap
 		}
-
-		if isNegated {
-			// invert(negate) the range first
-			invertedRunes := []int32{}
-			if re.Rune[0] > printableCharsLower {
-				invertedRunes = append(invertedRunes, printableCharsLower, int32(re.Rune[0]))
-			}
-			for i := 1; i < len(re.Rune)-1; i += 2 {
-				invertedRunes = append(invertedRunes, re.Rune[i]+1, re.Rune[i+1]-1)
-			}
-			if re.Rune[len(re.Rune)-1] < printableCharsUppper {
-				invertedRunes = append(invertedRunes, int32(re.Rune[len(re.Rune)-1]), printableCharsUppper)
-			}
-			return randomStringWithRange(invertedRunes), isNegated
+		return string(ru)
+	case syntax.OpAnyCharNotNL, syntax.OpAnyChar:
+		chars := printableChars
+		if op == syntax.OpAnyCharNotNL {
+			chars = printableChars
 		}
-		return randomStringWithRange(validRunes), isNegated // pick a random rune from range
-	case syntax.OpAnyChar, syntax.OpAnyCharNotNL:
-		// you cannot negate an AnyChar operator
-		return string([]byte{printableChars[utils.RandomIntWithRange(0, len(printableChars))]}), false
-	case syntax.OpBeginLine, syntax.OpEndLine, syntax.OpBeginText, syntax.OpEndText, syntax.OpWordBoundary, syntax.OpNoWordBoundary:
-		return "", false
+		c := chars[utils.RandomIntWithRange(0, len(chars))]
+		return string([]byte{c})
+	case syntax.OpBeginLine:
+	case syntax.OpEndLine:
+	case syntax.OpBeginText:
+	case syntax.OpEndText:
+	case syntax.OpWordBoundary:
+		return " "
+	case syntax.OpNoWordBoundary:
 	case syntax.OpCapture:
-		return generate(re.Sub0[0], not)
-	case syntax.OpStar: // Repeat zero or more times
-		limit := utils.RandomIntWithRange(0, repeatedstringLimit+1)
-		return repeatSubexpression(re, limit, not)
-	case syntax.OpPlus: // Repeat one or more times
-		limit := utils.RandomIntWithRange(0, repeatedstringLimit) + 1
-		isNegated = false
-		return repeatSubexpression(re, limit, not)
-	case syntax.OpQuest: // Zero or one instances
-		limit := utils.RandomIntWithRange(0, 2) // repeat zero or one time
-		return repeatSubexpression(re, limit, not)
-	case syntax.OpRepeat: // Repeat re.Min to min(re.Max, repeatedstringLimit) times
-		max := 0
-		if re.Max < 0 { // for re.Max == -1
-			max = repeatedstringLimit
-		} else {
-			max = int(math.Min(float64(re.Max), float64(repeatedstringLimit)))
+		return generate(re.Sub0[0])
+	case syntax.OpStar:
+		// Repeat zero or more times
+		res := ""
+		count := utils.RandomIntWithRange(0, repeatedstringLimit+1)
+		for i := 0; i < count; i++ {
+			for _, r := range re.Sub {
+				res += generate(r)
+			}
 		}
-		count := utils.RandomIntWithRange(int(math.Min(float64(re.Min), float64(max))), max+1)
-		return repeatSubexpression(re, count, not)
+		return res
+	case syntax.OpPlus:
+		// Repeat one or more times
+		res := ""
+		count := utils.RandomIntWithRange(0, repeatedstringLimit) + 1
+		for i := 0; i < count; i++ {
+			for _, r := range re.Sub {
+				res += generate(r)
+			}
+		}
+		return res
+	case syntax.OpQuest:
+		// Zero or one instances
+		res := ""
+		count := utils.RandomIntWithRange(0, 2)
+		for i := 0; i < count; i++ {
+			for _, r := range re.Sub {
+				res += generate(r)
+			}
+		}
+		return res
+	case syntax.OpRepeat:
+		// Repeat one or more times
+		res := ""
+		count := 0
+		re.Max = int(math.Min(float64(re.Max), float64(repeatedstringLimit)))
+		if re.Max > re.Min {
+			count = utils.RandomIntWithRange(0, re.Max-re.Min+1)
+		}
+		for i := 0; i < re.Min || i < (re.Min+count); i++ {
+			for _, r := range re.Sub {
+				res += generate(r)
+			}
+		}
+		return res
 	case syntax.OpConcat:
-		return repeatSubexpression(re, 1, not)
+		// Concatenate sub-regexes
+		res := ""
+		for _, r := range re.Sub {
+			res += generate(r)
+		}
+		return res
 	case syntax.OpAlternate:
-		randomIndex := utils.RandomIntWithRange(0, len(re.Sub))
-		return generate(re.Sub[randomIndex], not)
+		i := utils.RandomIntWithRange(0, len(re.Sub))
+		return generate(re.Sub[i])
 	default:
-		panic("Unhandled Operation")
+		log.Fatalf("[reg-gen] Unhandled op: %s", op.String())
 	}
+	return ""
 }
 
 // Generate a negated string from something
 func GenerateStringFromRegex(expression string, not bool) (res string, err error) {
-	re, err := syntax.Parse(expression, syntax.Perl)
+	re, err := syntax.Parse(expression, syntax.PerlX)
+	if err != nil {
+		return "", err
+	}
+	regex, err := regexp.Compile(expression)
 	if err != nil {
 		return "", err
 	}
 	if not {
-		isNegated := false
-		for !isNegated {
-			res, isNegated = generate(re, not)
+		for i := 0; i < maxRetry; i++ {
+			res = utils.RandomString(10)
+			if regex.MatchString(res) {
+				break
+			}
+		}
+		if !regex.MatchString(res) {
+			return "", ErrFailedGeneration
 		}
 	} else {
-		res, _ = generate(re, not)
+		res = generate(re)
+		replacer := strings.NewReplacer("K", " ", "ſ", " ") // case-folding
+		res = replacer.Replace(res)
 	}
 
 	return res, nil
